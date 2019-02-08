0bc42baf88b7567d7eaf68ac2c8829e6974386f3
Steven Duong <steven.duong@student.csulb.edu>
2019-02-07 19:10:17 -0800

    Updated

--- page3.php
+++ page3.php
@@ -1,73 +1,36 @@
 <!DOCTYPE html>
 
-
 <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>View Names</title>
+    </head>
+    <body>
+        <?php
+			$con = mysqli_connect("localhost","root");
+			mysqli_select_db($con, "employee");
+			$query = "SELECT * FROM employee";
+			$result = mysqli_query($con, $query);
+            echo "<table border=1 ><th>ID</th><th>Name</th>\n";
+            $none = 0;
+            while($row=mysqli_fetch_row($result)) {
+                $none=1;
+                printf("<tr><td><A href='page4.php?empID={$row[0]}'><span class=\"linkline\">%d</span></a> </td><td>%s</td></tr>\n",$row[0],$row[1]);
+            }
+        ?>
+        </table>
+        <?php
+            if($none==0)
+		Echo "<h3>No matching records found.</h3>\n";
+        ?>
+        <p><INPUT type="button" value="Cancel" id=button1 name=button1 onclick="go_home()">
+        <script language=Javascript>
 
-<head>
+            function go_home() {
+                window.location.replace("index.php");
+            }
 
-<meta charset="UTF-8">
-
-<title>View Names</title>
-
-</head>
-
-<body>
-
-<?php
-
-//MISSING connect to the database
-
-//MISSING select the database
-
-//MISSING construct query string for all the contents of people
-
-//MISSING execute the query
-
-echo "<table border=1 ><th>ID</th><th>First</th><th> Last</Th>\n";
-
-$none = 0;
-
-$con = mysqli_connect("localhost","root");
-mysqli_select_db($con, "lab2");
-$sql = "SELECT ID, first, last FROM people";
-$result = $con->query($sql);
-
-while($row=mysqli_fetch_row($result)) {
-
-$none=1;
-
-printf("<tr><td>%d</td><td>%s</td><td>%s</td></tr>\n",$row[0],$row[1],$row[2]);
-
-}
-
-?>
-
-</table>
-
-<?php
-
-if($none==0)
-
-Echo "<h3>No matching records found.</h3>\n";
-
-?>
-
-<p><INPUT type="button" value="Cancel" id=button1 name=button1 onclick="go_home()">
-
-<script language=Javascript>
-
-
-function go_home() {
-
-window.location.replace("index.php");
-
-}
-
-
-</script>
-
-</body>
-
+</script>    
+    </body>
 </html>
-
 
